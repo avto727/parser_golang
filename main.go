50f8c8431e9f8ebabe158c5001995899f470a46c
avto727 <avto727@bk.ru>
2019-12-01 13:53:02 +0300

    add comment to code

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
+	// Получаем аргумент из командной строки = url
 	for _, url := range os.Args[1:] {
 		links, err := findLinks(url)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "parse: %v/n", err)
+			fmt.Fprintf(os.Stderr, "main: %v/n", err)
 		}
 
 		for _, link := range links {
@@ -20,6 +21,7 @@ func main() {
 	}
 }
 
+//полцчаем код страницы url
 func findLinks(url string) ([]string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -28,18 +30,19 @@ func findLinks(url string) ([]string, error) {
 
 	if resp.StatusCode != http.StatusOK {
 		resp.Body.Close()
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+		return nil, fmt.Errorf("findLinks %s: %s", url, resp.Status)
 	}
 
 	doc, err := html.Parse(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+		return nil, fmt.Errorf("findLinks %s: %s", url, resp.Status)
 	}
 
 	return visit(nil, doc), nil
 }
 
+//выбор результатов со страницы url
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
